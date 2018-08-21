66d8dc4538bf720ed2b9f6a849bead1fa6cdc3cf
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-20 21:11:40 -0500

    format message

--- bot.py
+++ bot.py
@@ -169,8 +169,8 @@ async def manage(context):
 
 @client.command(pass_context=True)
 async def paypal(context):    
-    embed=discord.Embed(title="PayPal Link", url="https://www.paypal.me/jrdnm", description="To get text notifications for **@Hundy Chaser** pay $1 per month to this PayPal link. Add your discord name as the message for the payment. If you are signing up for the first time, DM @Aydenandjordan your cell phone number. Questions or problems? DM @Aydenandjordan", color=0x08d83b)
-    embed.set_author(name="@Hundy Chaser Text Message Notifications.")
+    embed=discord.Embed(title="PayPal Link", url="https://www.paypal.me/jrdnm", description="To get text notifications for **@Hundy Chaser** pay $1 per month to this PayPal link. Add your discord name as the message for the payment. If you are signing up for the first time, DM @Aydenandjordan your cell phone number.", color=0x08d83b)
+    embed.set_author(name="@Hundy Chaser Text Notifications")
     await client.send_message(context.message.author, embed=embed)
 
 
