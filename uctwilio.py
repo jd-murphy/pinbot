7b6b4b0073ae74da1c678a457c7c9360940555b6
Jordan Murphy <jordan@blueberri-saas.com>
2018-09-19 00:21:42 -0500

    removed payment reminder function

--- uctwilio.py
+++ uctwilio.py
@@ -9,48 +9,6 @@ twilioClient = Client(account_sid, auth_token)
 
 
 
-def paymentReminder():
-    status = 'Tagged'
-    totalMessagesSent = 0
-    try:
-        status = 'Connected'
-        print('calling twilio api..')
-        nums = []
-        users = pyrebase_worker.getData()
-        for user in users.each():
-            # print(user.key())
-            # print(user.val())
-            userDict = user.val()
-            print("userDict[\"name\"] -> " + userDict["name"])
-            print("userDict[\"phone\"] -> " + userDict["phone"])
-            nums.append(userDict["phone"])
-       
-
-
-        messageBody = "This is a payment reminder for the @Hundy Chaser text notifications. If you have already paid you can ignore this message. If you wish to continue the service for another month please pay $1 by PayPal to https://www.paypal.me/jrdnm   Or you can call the command '$paypal' in the discord server to get the paypal link. If you wish to opt out of the service please private message @Aydenandjordan in discord. Thanks!"
-        
-
-        for num in nums:
-            if num != "979-575-1540" and num != "979-402-3130‬" and num != "210-464-8654" and num != "830-832-2655":
-                twilioMessage = twilioClient.messages.create(
-                        body=messageBody,
-                        from_=environ['from'],
-                        to=num
-                    )
-                totalMessagesSent += 1
-
-        print('Payment reminder message sent - twilioMessage.sid -> ' + str(twilioMessage.sid))
-        pyrebase_worker.log("SMS payment reminder sent. Contents: " + messageBody)
-        status = 'Successfully sent ' + str(totalMessagesSent) + ' messages.'
-         
-    except Exception as e:
-        status = "ERROR!"
-        print('ERROR sending twilio payment reminder! -> ' + str(e))
-    finally:
-        print('Done: paymentReminder  -  Status: ' + status)
-
-
-
 
 def report3TS(message):
     status = 'Tagged'
